7be51e136f6006e500d2d3d379c7cbaa1ed0e5b9
achitivac <achitivac@outlook.com>
2023-10-31 16:20:12 -0500

    Reestructured repo

---
+++ src/main/java/org/codelab/softwaresol/SoftwaresolApplication.java
@@ -0,0 +1,13 @@
+package org.codelab.softwaresol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwaresolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwaresolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codelab/softwaresol/SoftwaresolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codelab.softwaresol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwaresolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
