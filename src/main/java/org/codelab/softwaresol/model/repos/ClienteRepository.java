02b9be48c9e9b800967e84ea1ffb01a5f2d6fc25
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2023-11-01 16:49:00 -0500

    Push on master little changes on -repos

--- src/main/java/org/codelab/softwaresol/model/repos/ClienteRepository.java
+++ src/main/java/org/codelab/softwaresol/model/repos/ClienteRepository.java
@@ -1,4 +1,4 @@
-package org.codelab.softwaresol.repos;
+package org.codelab.softwaresol.model.repos;
 
 import org.codelab.softwaresol.model.domain.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/org/codelab/softwaresol/services/ProductoService.java
+++ src/main/java/org/codelab/softwaresol/services/ProductoService.java
@@ -1,17 +1,19 @@
-package org.codelab.softwaresol.services;
-
-import org.codelab.softwaresol.model.domain.producto.Producto;
-import org.codelab.softwaresol.repos.ProductoRepository;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class ProductoService {
-    private ProductoRepository productoRepository;
-
-    public List<Producto> obtenerDatosBd(){
-        return productoRepository.findAll();
-
-    }
-}
+//package org.codelab.softwaresol.services;
+//
+//import org.codelab.softwaresol.model.domain.producto.Producto;
+//import org.codelab.softwaresol.model.repos.ProductoRepository;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class ProductoService {
+//    private ProductoRepository productoRepository;
+//
+//    @Bean
+//    public List<Producto> obtenerDatosBd() {
+//        return productoRepository.findAll();
+//
+//    }
+//}
