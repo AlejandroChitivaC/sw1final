777a3d5d784d411329352de07d5fdb4ca19acc2d
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2023-11-02 16:41:41 -0500

    Changing branch

--- src/main/java/org/codelab/softwaresol/SoftwaresolApplication.java
+++ src/main/java/org/codelab/softwaresol/SoftwaresolApplication.java
@@ -11,6 +11,12 @@ import java.net.URISyntaxException;
 @SpringBootApplication
 public class SoftwaresolApplication {
     public static void main(String[] args) throws URISyntaxException, IOException {
+
+        // URL que deseas pasar como argumento
+        String url = "http://localhost:8080/web/login.html";
+
+        // Crear una instancia de ApplicationArguments con la URL como argumento
+        //mandar como parametro del run la url (http://localhost:8080/web/login.html)
         SpringApplication.run(SoftwaresolApplication.class, args);
     }
 
