d2a6713f3477086083bb7fc3da2542dadf8ae831
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2023-11-02 22:25:44 -0500

    Working on frontend, login partially functional

---
+++ src/main/resources/static/web/scripts/login.js
@@ -0,0 +1,36 @@
+// var user = document.getElementById('username').value;
+// var pass = document.getElementById('pass').value;
+// var btn = document.getElementById('submit')
+
+// const data = {
+//     username: user,
+//     pass: pass
+// };
+
+
+// Función que se ejecutará cuando se envíe el formulario
+function handleSubmit(event) {
+    event.preventDefault(); // Evita que el formulario se envíe
+  
+    // Obtén los valores de los campos del formulario
+    var user = document.getElementById('username').value;
+    var pass = document.getElementById('pass').value;
+    var data = {
+      username: user,
+      pass: pass
+    };
+  
+    // Realiza la solicitud POST
+    axios.post('http://localhost:8080/auth/login', data)
+      .then(function (response) {
+          Swal.fire('Respuesta exitosa:', response.data);
+          window.location.href="/web/main.html"
+      })
+      .catch(function (error) {
+        // Maneja errores (puedes mostrar un mensaje de error o hacer lo que necesites aquí)
+        Swal.fire('Error:', error.data);
+      });
+  }
+  
+  // Asigna la función al evento onsubmit del formulario
+  document.getElementById('form').addEventListener('submit', handleSubmit);
