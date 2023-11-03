d9f905cd9ebc56ac88c4351bb6e335112d4d2f41
Towas <tespitia@unbosque.edu.co>
2023-11-03 17:33:14 -0500

    fixing errors

--- src/main/java/org/codelab/softwaresol/model/entities/detalles/DetalleVenta.java
+++ src/main/java/org/codelab/softwaresol/model/entities/detalles/DetalleVenta.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.codelab.softwaresol.model.entities.Venta;
+import org.codelab.softwaresol.model.entities.producto.Producto;
 
 @Getter
 @Setter
