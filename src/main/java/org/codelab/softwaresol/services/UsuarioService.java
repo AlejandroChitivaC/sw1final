fa3570d44265a9fcf86591945da889a8145bff7c
kxrojas <kxrojas@unbosque.edu.co>
2023-11-20 21:54:49 -0500

    cambios minimos

--- src/main/java/org/codelab/softwaresol/services/UsuarioService.java
+++ src/main/java/org/codelab/softwaresol/services/UsuarioService.java
@@ -1,15 +1,32 @@
 package org.codelab.softwaresol.services;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.codelab.softwaresol.model.entities.repos.UsuarioRepository;
 import org.codelab.softwaresol.model.entities.user.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.io.ByteArrayOutputStream;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 @Service
 public class UsuarioService {
+
+    @Autowired
+    private SpringTemplateEngine springTemplateEngine;
+
+    @Autowired
+    private ViewResolver viewResolver;
+
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -37,5 +54,4 @@ public class UsuarioService {
     public Usuario getById(int id){
         return usuarioRepository.findById(id).get();
     }
-
 }
